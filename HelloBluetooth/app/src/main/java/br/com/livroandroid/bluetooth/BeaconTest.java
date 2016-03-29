646a406856fbc160418f4042df2bdfabff2aefe3
Thiago Raulino Dal Pont <thiago.dalpont@outlook.com>
2016-03-29 09:55:43 -0300

    Troca da exibição do ibeacon detect. em string por listview

--- HelloBluetooth/app/src/main/java/br/com/livroandroid/bluetooth/BeaconTest.java
+++ HelloBluetooth/app/src/main/java/br/com/livroandroid/bluetooth/BeaconTest.java
@@ -4,41 +4,50 @@ import android.os.Bundle;
 import android.os.RemoteException;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
-import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 
 public class BeaconTest extends ActionBarActivity implements BeaconConsumer {
     private final String TAG = "BeaconTest";
     private final String REGION1 = "Region1";                               // Região
-    private final String UUID1 = "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0";    // UUID do sensor beacon
 
-    private TextView textView;
+    private long count = 0;
     private BeaconManager beaconManager;
+
+    private ListView listView;
+    private List<Beacon> devices;
+
+
     // -------- Activity methods -------- //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_beacon_test);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);      // Exibe o ícone de voltar à activity principal
-
+        setContentView(R.layout.activity_lista_devices);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);      // Exibe o ícone de voltar à activity principal
+        listView = (ListView) findViewById(R.id.listView);
+        devices = new ArrayList<>();
         //bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        textView = (TextView) findViewById(R.id.textView);
 
         // ---- Beacon cfg ---- //
         beaconManager = BeaconManager.getInstanceForApplication(this);
         beaconManager.setBackgroundMode(true);
+        beaconManager.setBackgroundScanPeriod(1000l);
+        beaconManager.setBackgroundBetweenScanPeriod(1l);
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
         beaconManager.bind(this);
     }
@@ -48,6 +57,28 @@ public class BeaconTest extends ActionBarActivity implements BeaconConsumer {
 
         beaconManager.unbind(this);
         super.onDestroy();
+    }
+
+    /**
+     * Atualização da lista de dispositivos
+     */
+    private void updateLista() {
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                List<String> infos = new ArrayList<String>();
+                for (Beacon beacon : devices) {
+                    infos.add("Device:\t\t" + beacon.getBluetoothName() + "\nMAC:\t\t\t" + beacon.getBluetoothAddress() + "\nUUID:\t\t" + beacon.getId1() +
+                            "\nMajor:\t\t" + beacon.getId2() + "\nMinor:\t\t" + beacon.getId3() + "\nRssi:\t\t\t" + beacon.getRssi());
+                }
+
+                int layout = android.R.layout.simple_list_item_1;
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(), layout, infos);
+
+                listView.setAdapter(adapter);
+            }
+        });
     }
 
     // -------- Beacon methods -------- //
@@ -79,23 +110,27 @@ public class BeaconTest extends ActionBarActivity implements BeaconConsumer {
 
                 Log.d(TAG, "SIZE:\t" + collection.size());
                 if (collection.size() > 0) {
+                    devices.clear();
                     for (Beacon beacon : collection) {
                         String message = beacon.getBluetoothName() + "/" + beacon.getBluetoothAddress() + "/" +
-                                beacon.getId1() + "/" + beacon.getId2() + "/" + beacon.getId3() + "/" + beacon.getRssi() + "/" + beacon.getDistance();
-                        String msg = message.replace('/', '\n');
+                                beacon.getId1() + "/" + beacon.getId2() + "/" + beacon.getId3() + "/" + beacon.getRssi() + "/" +
+                                beacon.getDistance();
 
                         Log.d(TAG, message);
-                        setText(textView, msg);
+
+
+                        devices.add(beacon);
+
                     }
                 }
+
+                updateLista();
             }
         });
 
         try {
-            Identifier identifier = Identifier.parse(UUID1); //beacon 1
-
-            beaconManager.startRangingBeaconsInRegion(new Region(REGION1, identifier, null, null));
-            beaconManager.startMonitoringBeaconsInRegion(new Region(REGION1, identifier, null, null));
+            beaconManager.startRangingBeaconsInRegion(new Region(REGION1, null, null, null));
+            beaconManager.startMonitoringBeaconsInRegion(new Region(REGION1, null, null, null));
         } catch (RemoteException e) {
             Log.e(TAG, e.getMessage(), e);
         }
@@ -109,7 +144,7 @@ public class BeaconTest extends ActionBarActivity implements BeaconConsumer {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                text.setText(value);
+                text.setText(++count + "\n\n" + value);
             }
         });
     }
